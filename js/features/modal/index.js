d5691e8f4f084a6ec5362b6cde195118dbdbfdc3
Zhila <ministrov2018@gmail.com>
2026-03-25 17:43:29 +0300

    feat(modal): end up creating a realisation of opening and closing modal with data attribute data modal

--- js/features/modal/index.js
+++ js/features/modal/index.js
@@ -1,6 +1,40 @@
 import getScrollBarWidth from '../../utils/getScrollBarWidth.js';
 
 export function initModal($) {
-  const scrollBarWidth = getScrollBarWidth($);
-  console.log(`initModal: ${$} ${scrollBarWidth}`);
+  // const scrollBarWidth = getScrollBarWidth($);
+
+  window.openPopup = function (popupId) {
+    $('#' + popupId).addClass('open');
+
+    const scrollbarWidth = getScrollBarWidth($);
+    document.body.style.overflow = 'hidden';
+    document.body.style.marginRight = `${scrollbarWidth}px`;
+  };
+
+  window.closePopup = function () {
+    $('.popup').removeClass('open');
+
+    setTimeout(() => {
+      document.body.style.overflow = '';
+      document.body.style.marginRight = '';
+    }, 300);
+  };
+
+  $('[data-modal]').on('click', function (e) {
+    e.preventDefault();
+    const modalId = $(this).data('modal');
+    openPopup(modalId);
+  });
+
+  $('.modal__close').on('click', closePopup);
+
+  $('.popup').on('click', function (e) {
+    if (e.target === this) {
+      closePopup();
+    }
+  });
+
+  $(document).on('keyup', function (e) {
+    if (e.key === 'Escape') closePopup();
+  });
 }
