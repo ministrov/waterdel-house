89d4c4f057acf9af6f7cc03a50688b1d24dca3ab
Zhila <ministrov2018@gmail.com>
2026-03-25 18:56:39 +0300

    feat(faq): end up creating an open and close realisaton of an faq accordion with a jquery slideUpand sliderDown functions

--- js/features/faq/index.js
+++ js/features/faq/index.js
@@ -1,3 +1,15 @@
 export function initFaq($) {
-  console.log('initFaq');
+  $('.faq__question-top').on('click', function () {
+    const $this = $(this);
+
+    if ($this.hasClass('open')) {
+      $this.removeClass('open');
+      $this.next().slideUp(300);
+    } else {
+      $('.faq__question-top').removeClass('open');
+      $('.faq__question-answer').slideUp(300);
+      $this.addClass('open');
+      $this.next().slideDown(300);
+    }
+  });
 }
