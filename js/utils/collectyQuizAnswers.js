f9b4af070a9d27f3ff107f75224d02d7fab6d6b2
Zhila <ministrov2018@gmail.com>
2026-03-26 13:47:48 +0300

    feat(quiz): create and write a util function to create an array of answers

---
+++ js/utils/collectyQuizAnswers.js
@@ -0,0 +1,16 @@
+export function collectQuizAnswers($, $rows) {
+  const answers = [];
+
+  $rows.not('[dat-step="final"]').each(function () {
+    const $row = $(this);
+    const question = $row.find('.quiz-form__left-title').text().trim();
+    const $checked = $row.find('input:checked');
+    const values = $checked.map(() => $(this).val()).get();
+
+    if (values.length > 0) {
+      answers.push({ question, answers: values });
+    }
+  });
+
+  return answers;
+}
