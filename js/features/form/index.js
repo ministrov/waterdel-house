b6f871b14e381c63de13f550261b727971a1e7aa
Zhila <ministrov2018@gmail.com>
2026-03-27 10:31:54 +0300

    feat(form): connect all modules to an entry point of initForm

--- js/features/form/index.js
+++ js/features/form/index.js
@@ -1,7 +1,15 @@
 import { maksPhone } from './maskPhone.js';
+import { sanitizeName } from './sanitizeName.js';
+// import { validateForm } from './validator.js';
+// import { getFormData } from './collectore.js';
+// import { sendForm } from './sender.js';
+// import { addNotifications } from './notification.js';
 
 export function initForm($) {
   console.log('initForm');
 
   maksPhone($);
+
+  // Sanitize name input
+  sanitizeName($);
 }
